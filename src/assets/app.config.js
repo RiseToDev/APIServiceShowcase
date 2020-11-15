3d341c3fa5859ca78e54f955aab0b67f5d3c5907
RiseToDev <ram14apple@gmail.com>
2020-11-15 23:10:05 +0200

    added mocks

--- src/assets/app.config.js
+++ src/assets/app.config.js
@@ -1,5 +1,5 @@
 (function (window) {
   window.appConfig = window.appConfig || {};
 
-  window.appConfig.baseApiUrl = "http://b259ab204ea3.ngrok.io/api";
+  window.appConfig.baseApiUrl = "https://4feae2633608.ngrok.io/api";
 })(this);
